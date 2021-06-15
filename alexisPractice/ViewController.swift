fcf4f8be55ad6a2dab33f866332a9462964b1165
alexis lee <alexis290@icloud.com>
2021-06-15 14:18:13 -0400

    Initial Commit

---
+++ alexisPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alexisPractice
+//
+//  Created by alexis lee on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
